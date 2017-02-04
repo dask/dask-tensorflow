e5d78f82c049c53a32ca63ed9752458292ff8701
Matthew Rocklin <mrocklin@gmail.com>
2017-02-04 18:23:03 -0500

    support job names in start_tensorflow

--- dask_tensorflow/core.py
+++ dask_tensorflow/core.py
@@ -15,32 +15,67 @@ def start_and_attach_server(spec, job_name=None, task_index=None, dask_worker=No
 
 
 @gen.coroutine
-def _start_tensorflow(client):
+def _start_tensorflow(client, **job_counts):
     info = yield client.scheduler.identity()
     if not info['workers']:
         return
 
+    if not job_counts:
+        job_counts = {'worker': len(info['workers'])}
+
+    if sum(job_counts.values()) > len(info['workers']):
+        raise ValueError("Dask cluster not large enough."
+                         "Need %d workers, have %d"
+                         % (sum(job_counts.values()), len(info['workers'])))
+
     ports = defaultdict(lambda: 2221)
-    spec = {'worker': []}
+    spec = {job_name: [] for job_name in job_counts}
+    job_names = {}
     task_index = {}
-    for i, w in enumerate(info['workers']):
-        host = parse_host_port(w)[0].strip('/')
-        ports[host] += 1
-        spec['worker'].append('%s:%d' % (host, ports[host]))
-        task_index[w] = i
+    worker_index = {}
+    workers = iter(info['workers'])
+    for job_name, count in job_counts.items():
+        for i in range(count):
+            w = next(workers)
+            host = parse_host_port(w)[0].strip('/')
+            ports[host] += 1
+            tf_name = '%s:%d' % (host, ports[host])
+            spec[job_name].append(tf_name)
+            task_index[w] = i
+            job_names[w] = job_name
+            worker_index[w] = tf_name
 
     spec = tf.train.ClusterSpec(spec)
 
     resp = yield {w: client._run(start_and_attach_server, spec,
-                                 job_name='worker',
+                                 job_name=job_names[w],
                                  task_index=task_index[w],
-                                 workers=[w]) for w in info['workers']}
+                                 workers=[w]) for w in task_index}
     resp = merge(resp.values())
     if not all(v == 'OK' for v in resp.values()):
         raise ValueError("Setup did not succeed")
 
-    return spec
+    return spec, worker_index
 
 
 def start_tensorflow(client, **kwargs):
+    """ Start Tensorflow on Dask Cluster
+
+    This launches Tensorflow Servers alongside Dask workers
+
+    Examples
+    --------
+    >>> client = Client('dask-scheduler-address:8786')
+    >>> spec, workers = start_tensorflow(client)
+    >>> spec.as_dict()
+    {'worker': ['192.168.1.100:2222', '192.168.1.101:2222']}
+
+    Specify desired number of jobs types as keyword args
+
+    >>> spec, workers = start_tensorflow(client, ps=2, worker=4)
+    >>> spec.as_dict()
+    {'worker': ['192.168.1.100:2222', '192.168.1.101:2222',
+                '192.168.1.102:2222', '192.168.1.103:2222'],
+     'ps': ['192.168.1.104:2222', '192.168.1.105:2222']}
+    """
     return sync(client.loop, _start_tensorflow, client, **kwargs)
--- dask_tensorflow/tests/test_core.py
+++ dask_tensorflow/tests/test_core.py
@@ -6,15 +6,28 @@ from dask_tensorflow import _start_tensorflow, start_tensorflow
 
 @gen_cluster(client=True)
 def test_basic(c, s, a, b):
-    spec = yield _start_tensorflow(c)
+    spec, d = yield _start_tensorflow(c)
     assert isinstance(a.tensorflow_server, tf.train.Server)
     assert isinstance(b.tensorflow_server, tf.train.Server)
     assert isinstance(spec, tf.train.ClusterSpec)
     assert sum(map(len, spec.as_dict().values())) == len(s.workers)
+    assert set(d) == set(s.workers)
 
 
 def test_basic_sync(loop):
     with cluster() as (s, [a, b]):
         with Client(s['address'], loop=loop) as c:
-            spec = start_tensorflow(c)
+            spec, d = start_tensorflow(c)
             assert isinstance(spec, tf.train.ClusterSpec)
+
+
+@gen_cluster(client=True, ncores=[('127.0.0.1', 1)] * 10)
+def test_user_defined_spec(c, s, *workers):
+    spec, d = yield _start_tensorflow(c, ps=2, worker=4)
+    assert sum(hasattr(w, 'tensorflow_server') for w in workers) == 6
+    spec = spec.as_dict()
+    assert len(spec['ps']) == 2
+    assert len(spec['worker']) == 4
+
+    assert set(d).issubset(s.workers)
+    import pdb; pdb.set_trace()
